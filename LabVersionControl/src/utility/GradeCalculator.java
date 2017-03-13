467bae987e56a21e506b3c1902e57e1b5e85766c
DESKTOP-9CKSS1Q\CAMT <kanatip_bu@cmu.ac.th>
2017-03-13 18:10:05 +0700

    Class GradeCalculator

--- LabVersionControl/src/utility/GradeCalculator.java
+++ LabVersionControl/src/utility/GradeCalculator.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
-
 /**
  * Created by CAMT on 3/13/2017.
  */
 public class GradeCalculator {
+    public char calculate(double... scores){
+        double sum = 0;
+        for (int i = 0 ; i < scores.length ; i++) {
+            sum = sum + scores[i];
+        }
+        if(sum <= 100 && sum >= 0){
+            if (sum > 95) return 'A';
+            if (sum > 80) return 'B';
+            if (sum > 60) return 'C';
+            return 'F';
+        }else {
+            return 'x';
+        }
+    }
 }
