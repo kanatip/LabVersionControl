00912d31d68dae6c13016c5367b6c4cf716d7452
DESKTOP-9CKSS1Q\CAMT <kanatip_bu@cmu.ac.th>
2017-03-13 17:12:26 +0700

    Class Currency

---
+++ LabVersionControl/src/utility/Currency.java
@@ -0,0 +1,9 @@
+
+/**
+ * Created by CAMT on 3/13/2017.
+ */
+public class Currency {
+    public double exchange(String sourceCurrency, double amount, String targetCurrency){
+        return 0.0;
+    }
+}
